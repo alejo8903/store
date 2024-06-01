4ec783fbaebfb33c86871f4e1d949c1550347aee
AMIRA\alejo <alejo890327@gmail.com>
2024-05-31 19:08:12 -0500

    funcion inventario

--- src/main/java/com/store/store/controller/HomeController.java
+++ src/main/java/com/store/store/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.store.store.controller;
 
 
+import com.store.store.entity.Producto;
+import com.store.store.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    @Autowired
+    private ProductoService productoService;
 
 
     @GetMapping("/")
@@ -17,8 +26,16 @@ public class HomeController {
     }
 
     @GetMapping("/inventario")
-    public String inventario() {
+    public String inventario(Model model) {
+        model.addAttribute("producto", new Producto());
+        model.addAttribute("productos", productoService.getAllProductos());
         return "fragments/inventario :: content";
+    }
+
+    @PostMapping("/inventario")
+    public String saveProducto(@ModelAttribute Producto producto) {
+        productoService.saveProducto(producto);
+        return "redirect:/inventario";
     }
 
     @GetMapping("/generar-pedido")
--- src/main/java/com/store/store/service/impl/ProductoServiceImpl.java
+++ src/main/java/com/store/store/service/impl/ProductoServiceImpl.java
@@ -1,2 +1,36 @@
-package com.store.store.service.impl;public class ProductoServiceImpl {
+package com.store.store.service.impl;
+
+import com.store.store.entity.Producto;
+import com.store.store.repository.ProductoRepository;
+import com.store.store.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @Override
+    public List<Producto> getAllProductos() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public Producto saveProducto(Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    public Producto getProductoById(int id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteProductoById(int id) {
+        productoRepository.deleteById(id);
+    }
 }
