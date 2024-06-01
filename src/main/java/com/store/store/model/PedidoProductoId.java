57ea3b237eb53885381c74c07d9f5cb11736943d
AMIRA\alejo <alejo890327@gmail.com>
2024-05-31 20:31:48 -0500

    cambio de campos en tablas

--- src/main/java/com/store/store/model/PedidoProductoId.java
+++ src/main/java/com/store/store/model/PedidoProductoId.java
@@ -4,7 +4,15 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Embeddable
 public class PedidosProductoPK implements Serializable {
 
