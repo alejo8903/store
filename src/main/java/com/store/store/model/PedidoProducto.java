06ff7893cc76780e069502b7ac8def915c18e42a
AMIRA\alejo <alejo890327@gmail.com>
2024-05-31 20:31:48 -0500

    cambio de campos en tablas

---
+++ src/main/java/com/store/store/model/PedidoProducto.java
@@ -0,0 +1,37 @@
+package com.store.store.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "pedidos_productos")
+public class PedidoProducto {
+
+    @EmbeddedId
+    private PedidoProductoId id;
+
+    @ManyToOne
+    @MapsId("idPedido")
+    @JoinColumn(name = "id_pedido")
+    private Pedido pedido;
+
+    @ManyToOne
+    @MapsId("idProducto")
+    @JoinColumn(name = "id_producto")
+    private Producto producto;
+
+    private Integer cantidad;
+
+    private Double total;
+
+
+}
+
